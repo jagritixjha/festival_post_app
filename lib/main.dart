11aae5653225eb6e9d7ed4c2098d601ac2d0919d
jagriti <im.jagriti.jha@gmail.com>
2024-04-25 18:33:05 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:festival_poster_app/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FestiveFrame App',
+      initialRoute: '/',
+      routes: Routes.screens,
+    );
+  }
+}
